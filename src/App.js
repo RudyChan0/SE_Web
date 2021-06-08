6c93842ee81d6f0ca10bc63e34aab2de9750b5f2
苗条俊 <13141699505>
2021-06-08 22:10:02 +0800

    massion 2&3 completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/employee">Employee</Link>
+            </li>
+            <li>
+              <Link to="/list">List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/employee">
+            <Employee />
+          </Route>
+          <Route path="/list">
+            <List />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,10 +59,16 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+          </a>
       </header>
     </div>
-    )
-  }
+  )
+}
 
-export default App;
+function Employee() {
+  return <h2>Employee</h2>;
+}
+
+function List() {
+  return <h2>List</h2>;
+}
