152b7fd45724dde8e4c000b834099188e610e70e
苗条俊 <13141699505>
2021-06-10 23:09:31 +0800

    try massion 4

--- src/list.js
+++ src/list.js
@@ -3,43 +3,79 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Table, Tag, Space } from 'antd';
+import { queryAllByAttribute } from '@testing-library/dom';
 
 const { Column, ColumnGroup } = Table;
-function Data(){
-    
-        const data = [
-            {
-                key: '1',
-                firstName: 'Joh',
-                lastName: 'Brown',
-                age: 32,
-                address: 'New York No. 1 Lake Park',
-                tags: ['nice', 'developer'],
-            },
-            {
-                key: '2',
-                firstName: 'Jim',
-                lastName: 'Green',
-                age: 42,
-                address: 'London No. 1 Lake Park',
-                tags: ['loser'],
-            },
-            {
-                key: '3',
-                firstName: 'Joe',
-                lastName: 'Black',
-                age: 32,
-                address: 'Sidney No. 1 Lake Park',
-                tags: ['cool', 'teacher'],
-            },
-        ];
-    
+const data = [
+    {
+        key: '1',
+        firstName: 'Joh',
+        lastName: 'Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+        tags: ['nice', 'developer'],
+    },
+    {
+        key: '2',
+        firstName: 'Jim',
+        lastName: 'Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+        tags: ['loser'],
+    },
+    {
+        key: '3',
+        firstName: 'Joe',
+        lastName: 'Black',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park',
+        tags: ['cool', 'teacher'],
+    },
+]
+
+function Data(deletekey) {
+
+    //return data
+/*     if(deletekey!=0){
+        data.splice(deletekey,1);
+        this.setstate({
+            data: data,
+        }
+        );
+        console.log(data)
+    } */
+/*     const { data } = this.state;
+    data.splice(deletekey,1);
+    this.setstate({
+        data: data,
+    }) */
+    if(deletekey!=0){
+    const newdata = data.splice(deletekey,1)
+    data=newdata
+    }
+    //this.setstate({data:newdata})
+/*     data.map((item, deletekey) => {
+        if (item === deletekey) {
+          data.splice(deletekey, 1);
+          this.setState({
+            data: data,
+          });
+  　　　　console.log(data) //这里打印发现data已经发生了变化，但是setState在页面上数据却不改变是不是很奇怪呢？
+        }
+      }); */
+    return data
 }
 
 
+
+
+
 export default function Editablelist() {
+
     return (
-        <Table dataSource={Data()}>
+
+        <Table dataSource={Data(0)}>
+            alert(record.key)
             <ColumnGroup title="Name">
                 <Column title="First Name" dataIndex="firstName" key="firstName" />
                 <Column title="Last Name" dataIndex="lastName" key="lastName" />
@@ -60,13 +96,14 @@ export default function Editablelist() {
                     </>
                 )}
             />
+
             <Column
                 title="Action"
                 key="action"
                 render={(text, record) => (
                     <Space size="middle">
                         <a>Edit {record.lastName}</a>
-                        <a><font color="#FF0000">Delete</font></a>
+                        <a onClick={Data.bind(this,record.key)}><font color="#FF0000">Delete</font></a>
                     </Space>
                 )}
             />
